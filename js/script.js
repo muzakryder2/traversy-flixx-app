b3dda5c1ba9d1f4555dcc886aae8a54f7798ec74
muzakryder2 <muzakryder2@msn.com>
2023-11-05 11:21:09 -0600

    added fetch movie details funtionality

--- js/script.js
+++ js/script.js
@@ -68,6 +68,74 @@ async function displayPopularShows() {
   })
 }
 
+// Display Movie Details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1]
+  const movie = await fetchApiData(`movie/${movieId}`)
+
+  const div = document.createElement('div')
+  div.innerHTML = `
+  <div class="details-top">
+    <div>
+    ${
+      movie.poster_path
+        ? `<img
+      src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`
+        : `
+    <img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${movie.title}"
+    />
+    `
+    }
+    </div>
+    <div>
+      <h2>${movie.title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${movie.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>
+        ${movie.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${
+        movie.homepage
+      }" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> ${currencyFormatter.format(
+        movie.budget
+      )}</li>
+      <li><span class="text-secondary">Revenue:</span> ${currencyFormatter.format(
+        movie.revenue
+      )}</li>
+      <li><span class="text-secondary">Runtime:</span> ${
+        movie.runtime
+      } minutes</li>
+      <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+    ${movie.production_companies
+      .map((company) => `<span>${company.name}, </span>`)
+      .join('')}
+    </div>
+  </div>`
+  document.querySelector('#movie-details').appendChild(div)
+}
+
 // Fetch data from TMDB API
 async function fetchApiData(endpoint) {
   const API_KEY = '8e97bad3b85c51d8e8e039b01d2f2f1f'
@@ -103,6 +171,14 @@ function highlightActiveLink() {
   })
 }
 
+// Format USD Currency
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
+})
+
 // Init App
 function init() {
   switch (global.currentPage) {
@@ -114,7 +190,7 @@ function init() {
       displayPopularShows()
       break
     case '/movie-details.html':
-      console.log('Movie Details')
+      displayMovieDetails()
       break
     case '/tv-details.html':
       console.log('TV Details')

f1ba8fbb703f4afad4a9dcd7c6a3bd83a40214c6
muzakryder2 <muzakryder2@msn.com>
2023-11-04 22:22:55 -0500

    Fetch and display popular movies funtionality complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,50 @@
 const global = { currentPage: window.location.pathname }
 
+async function displayPopularMovies() {
+  const { results } = await fetchApiData('movie/popular')
+  results.forEach((movie) => {
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+      ${
+        movie.poster_path
+          ? `<img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />`
+          : `
+      <img
+        src="images/no-image.jpg"
+        class="card-img-top"
+        alt="${movie.title}"
+      />
+      `
+      }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${movie.title}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${movie.release_date}</small>
+      </p>
+    </div>`
+    document.querySelector('#popular-movies').appendChild(div)
+  })
+}
+
+// Fetch data from TMDB API
+async function fetchApiData(endpoint) {
+  const API_KEY = '8e97bad3b85c51d8e8e039b01d2f2f1f'
+  const API_URL = 'https://api.themoviedb.org/3'
+
+  const response = await fetch(
+    `${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`
+  )
+  const data = await response.json()
+  return data
+}
+
 // Highlight active link
 function highlightActiveLink() {
   const navLinks = document.querySelectorAll('.nav-link')
@@ -15,7 +60,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home')
+      displayPopularMovies()
       break
     case '/shows.html':
       console.log('Shows')
